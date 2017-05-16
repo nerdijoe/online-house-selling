ea1cc9d340011851ff528c0b7368dcf2f3c82b3e
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-16 12:35:37 +0700

    post CRUD done

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -5,8 +5,35 @@ exports.create = (req, res, next) => {
 
   Post.create(req.body, (err, post) => {
     if(err) res.send(err)
-
-    res.send(post)
+    else {
+      res.send(post)
+    }
   })
 
+}
+
+exports.get_all = (req, res, next) => {
+  Post.find( (err, posts) => {
+    if(err) res.send(err)
+    res.send(posts)
+  })
+}
+
+exports.update = (req, res, next) => {
+  Post.findByIdAndUpdate(req.params.id, {$set: req.body}, { new: true} )
+  .exec( (err, post) => {
+    if(err) res.send(err)
+    else {
+      res.send(post);
+    }
+  })
+}
+
+exports.delete = (req, res, next) => {
+  Post.findByIdAndRemove(req.params.id, (err, post) => {
+    if(err) res.send(err)
+    else {
+      res.send(post)
+    }
+  })
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var postController = require('../controllers/postController')
 
 router.post('/', postController.create);
-
-
+router.get('/', postController.get_all);
+router.put('/:id', postController.update);
+router.delete('/:id', postController.delete);
 module.exports = router;
